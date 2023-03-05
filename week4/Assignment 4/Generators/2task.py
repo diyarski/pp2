fb3e5581fa6100dbd64d16a10143ac0be761f89a
diyarski <122877469+diyarski@users.noreply.github.com>
2023-03-06 02:13:15 +0600

    Create 2task.py

---
+++ week4/Assignment 4/Generators/2task.py
@@ -0,0 +1,10 @@
+def even_generator(n):
+    for i in range(n + 1):
+        if i % 2 == 0:
+            yield i
+
+n = int(input())
+
+even_numbers = even_generator(n)
+
+print(*even_numbers, sep=", ")
