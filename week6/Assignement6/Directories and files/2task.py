f98c9ced081b9ef028d568a31de468492a0e4572
diyarski <122877469+diyarski@users.noreply.github.com>
2023-03-09 17:19:27 +0600

    Update 2task.py

--- week6/Assignement6/Directories and files/2task.py
+++ week6/Assignement6/Directories and files/2task.py
@@ -4,8 +4,25 @@ path = "C:/Users/dtbku/Documents/uni"
 
 # Check if path exists
 if os.path.exists(path):
-    directory, filename = os.path.split(path)
-    print("Directory: ", directory)
-    print("Filename: ", filename)
+    print("Path exists")
+
+    # Check if path is readable
+    if os.access(path, os.R_OK):
+        print("Path is readable")
+    else:
+        print("Path is not readable")
+
+    # Check if path is writable
+    if os.access(path, os.W_OK):
+        print("Path is writable")
+    else:
+        print("Path is not writable")
+
+    # Check if path is executable
+    if os.access(path, os.X_OK):
+        print("Path is executable")
+    else:
+        print("Path is not executable")
+
 else:
-    print("Path does not exists")
+    print("Path does not exist")
